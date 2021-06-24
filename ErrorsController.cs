f036b1e66b75fe8cb3b4711aa4a8ae5a74600a31
Gordon Kenyon <gordonkenyon@users.noreply.github.com>
2021-06-24 12:16:47 +1000

    Create ErrorsController.cs

---
+++ ErrorsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SageAPI.V2.Controllers
+{
+    [ApiVersion("3")]
+    [Authorize]
+    [Route("v{version:apiVersion}/api/[controller]")]
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class GlobalErrorsController : ControllerBase
+    {
+        [HttpGet]       
+        [Route("/errors")]
+        public IActionResult HandleErrors()
+        {
+            var contextException = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            var responseStatusCode = contextException.Error.GetType().Name switch
+            {
+                "NullReferenceException" => HttpStatusCode.BadRequest , _=> HttpStatusCode.ServiceUnavailable
+            };
+
+            return Problem(detail: contextException.Error.Message, statusCode: (int) responseStatusCode);
+        }
+
+    }
+}
